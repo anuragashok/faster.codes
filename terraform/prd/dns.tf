82c698a8b31bf3be9ad3e0b20bbc85aaf59da239
Anurag Ashok <anurag16890@gmail.com>
2021-09-24 08:26:30 +0000

    add custom domain to web

--- terraform/prd/dns.tf
+++ terraform/prd/dns.tf
@@ -3,18 +3,16 @@ resource "cloudflare_zone" "faster_codes" {
 }
 
 resource "cloudflare_record" "faster_codes_web" {
-  zone_id = var.cloudflare_zone.faster_codes.id
+  zone_id = cloudflare_zone.faster_codes.id
   name    = "@"
   value   = "faster-codes.pages.dev"
   type    = "CNAME"
   ttl     = 1
 }
-output "faster_codes_zone_id" {
-  value = cloudflare_zone.faster_codes.id
-}
+
 
 resource "cloudflare_record" "faster_codes_web_www" {
-  zone_id = var.faster_codes_zone_id
+  zone_id = cloudflare_zone.faster_codes.id
   name    = "www"
   value   = "faster-codes.pages.dev"
   type    = "CNAME"
