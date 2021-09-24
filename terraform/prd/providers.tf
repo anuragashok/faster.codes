0306fadff7a5105330f351e3e33234ea62e3583c
Anurag Ashok <anurag16890@gmail.com>
2021-09-24 08:15:29 +0000

    add custom domain to web

--- terraform/prd/providers.tf
+++ terraform/prd/providers.tf
@@ -8,17 +8,20 @@ terraform {
       source = "cloudflare/cloudflare"
       version = "3.1.0"
     }
+    namecheap = {
+      source = "Namecheap-Ecosystem/namecheap"
+      version = "0.1.7"
+    }
   }
 }
 
 variable "PROVIDER_DIGITALOCEAN_TOKEN" {}
-variable "PROVIDER_CLOUDFLARE_EMAIL" {}
-variable "PROVIDER_CLOUDFLARE_API_KEY" {}
-
 provider "digitalocean" {
   token = var.PROVIDER_DIGITALOCEAN_TOKEN
 }
 
+variable "PROVIDER_CLOUDFLARE_EMAIL" {}
+variable "PROVIDER_CLOUDFLARE_API_KEY" {}
 provider "cloudflare" {
   email   = var.PROVIDER_CLOUDFLARE_EMAIL
   api_key = var.PROVIDER_CLOUDFLARE_API_KEY
