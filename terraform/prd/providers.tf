cd87c103aab8938790e5ca31bd1c94f5d8e0a340
Anurag Ashok <anuragashok@users.noreply.github.com>
2021-09-29 19:19:34 +0800

    add backend scaleway k8s (#6)

--- terraform/prd/providers.tf
+++ terraform/prd/providers.tf
@@ -1,12 +1,15 @@
 terraform {
   required_providers {
     digitalocean = {
-      source = "digitalocean/digitalocean"
+      source  = "digitalocean/digitalocean"
       version = "~> 2.0"
     }
     cloudflare = {
-      source = "cloudflare/cloudflare"
+      source  = "cloudflare/cloudflare"
       version = "3.1.0"
+    }
+    scaleway = {
+      source = "scaleway/scaleway"
     }
   }
 }
@@ -18,7 +21,12 @@ provider "digitalocean" {
 
 variable "PROVIDER_CLOUDFLARE_EMAIL" {}
 variable "PROVIDER_CLOUDFLARE_API_KEY" {}
+variable "PROVIDER_CLOUDFLARE_ACCOUNT_ID" {}
 provider "cloudflare" {
-  email   = var.PROVIDER_CLOUDFLARE_EMAIL
-  api_key = var.PROVIDER_CLOUDFLARE_API_KEY
+  email      = var.PROVIDER_CLOUDFLARE_EMAIL
+  api_key    = var.PROVIDER_CLOUDFLARE_API_KEY
+  account_id = var.PROVIDER_CLOUDFLARE_ACCOUNT_ID
 }
+
+#configured using env vars SCW_ACCESS_KEY, SCW_SECRET_KEY, SCW_DEFAULT_PROJECT_ID
+provider "scaleway" {}
