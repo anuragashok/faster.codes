3854c5e2141d918c92f928c8e4d38f193e082ca1
Anurag Ashok <anurag16890@gmail.com>
2021-09-30 11:38:13 +0000

    init backend-api hello world

--- backend/api/src/main.ts
+++ backend/api/src/main.ts
@@ -1,32 +1,12 @@
-/**
- * Some predefined delay values (in milliseconds).
- */
-export enum Delays {
-  Short = 500,
-  Medium = 2000,
-  Long = 5000,
-}
+import Koa from 'koa';
+import * as Router from '@koa/router';
 
-/**
- * Returns a Promise<string> that resolves after a given time.
- *
- * @param {string} name - A name.
- * @param {number=} [delay=Delays.Medium] - A number of milliseconds to delay resolution of the Promise.
- * @returns {Promise<string>}
- */
-function delayedHello(
-  name: string,
-  delay: number = Delays.Medium,
-): Promise<string> {
-  return new Promise((resolve: (value?: string) => void) =>
-    setTimeout(() => resolve(`Hello, ${name}`), delay),
-  );
-}
+const app = new Koa();
+const router = new Router();
 
-// Below are examples of using ESLint errors suppression
-// Here it is suppressing a missing return type definition for the greeter function.
+router.get('/run', (ctx) => {
+  ctx.body = 'Hello World';
+});
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export async function greeter(name: string) {
-  return await delayedHello(name, Delays.Long);
-}
+app.use(router.routes()).use(router.allowedMethods());
+app.listen(3000);
