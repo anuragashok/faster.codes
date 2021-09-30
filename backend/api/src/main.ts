66908a843e10c7662b85cc32ce53882a08ac6cb0
Anurag Ashok <anurag16890@gmail.com>
2021-09-30 11:57:13 +0000

    add backend api

--- backend/api/src/main.ts
+++ backend/api/src/main.ts
@@ -5,7 +5,7 @@ const app = new Koa();
 const router = new Router();
 
 router.get('/run', (ctx) => {
-  ctx.body = 'Hello World';
+  ctx.body = 'Hello World2';
 });
 
 app.use(router.routes()).use(router.allowedMethods());
