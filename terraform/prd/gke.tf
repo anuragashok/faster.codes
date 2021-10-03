8c75b0131823dcab6086e72fab8712a163d2f45f
Anurag Ashok <anurag16890@gmail.com>
2021-10-03 10:48:12 +0000

    setup workflow

--- terraform/prd/gke.tf
+++ terraform/prd/gke.tf
@@ -40,6 +40,7 @@ module "gke" {
   name                   = "${var.cluster_name}"
   network                = module.gcp-network.network_name
   subnetwork             = module.gcp-network.subnets_names[0]
+  project_id             = var.project_id
   ip_range_pods          = var.ip_range_pods_name
   ip_range_services      = var.ip_range_services_name
   node_pools = [
