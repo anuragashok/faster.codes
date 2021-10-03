bd80c1a3485ad8aa7d73a4e360c75040dfb582cb
Anurag Ashok <anurag16890@gmail.com>
2021-10-03 10:42:14 +0000

    setup workflow

--- terraform/prd/gke.tf
+++ terraform/prd/gke.tf
@@ -1,14 +1,54 @@
-# Retrieve an access token as the Terraform runner
-data "google_client_config" "provider" {}
-
-data "google_container_cluster" "faster_codes_backend" {
-  name     = "faster-codes-backend"
+odule "gke_auth" {
+  source = "terraform-google-modules/kubernetes-engine/google//modules/auth"
+  depends_on   = [module.gke]
+  project_id   = var.project_id
+  location     = module.gke.location
+  cluster_name = module.gke.name
+}
+resource "local_file" "kubeconfig" {
+  content  = module.gke_auth.kubeconfig_raw
+  filename = "kubeconfig"
+}
+module "gcp-network" {
+  source       = "terraform-google-modules/network/google"
+  version      = "~> 2.5"
+  project_id   = var.project_id
+  network_name = "${var.network}"
+  subnets = [
+    {
+      subnet_name   = "${var.subnetwork}"
+      subnet_ip     = "10.10.0.0/16"
+      subnet_region = var.region
+    },
+  ]
+  secondary_ranges = {
+    "${var.subnetwork}-${var.env_name}" = [
+      {
+        range_name    = var.ip_range_pods_name
+        ip_cidr_range = "10.20.0.0/16"
+      },
+      {
+        range_name    = var.ip_range_services_name
+        ip_cidr_range = "10.30.0.0/16"
+      },
+    ]
+  }
 }
 
-provider "kubernetes" {
-  host  = "https://${data.google_container_cluster.my_cluster.endpoint}"
-  token = data.google_client_config.provider.access_token
-  cluster_ca_certificate = base64decode(
-    data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate,
-  )
+module "gke" {
+  source                 = "terraform-google-modules/kubernetes-engine/google//modules/private-cluster"
+  name                   = "${var.cluster_name}"
+  network                = module.gcp-network.network_name
+  subnetwork             = module.gcp-network.subnets_names[0]
+  ip_range_pods          = var.ip_range_pods_name
+  ip_range_services      = var.ip_range_services_name
+  node_pools = [
+    {
+      name                      = "node-pool"
+      machine_type              = "e2-medium"
+      min_count                 = 1
+      max_count                 = 2
+      disk_size_gb              = 30
+    },
+  ]
 }
--- terraform/prd/variables.tf
+++ terraform/prd/variables.tf
@@ -1,1 +1,26 @@
 variable "backend-api-tag" {}
+
+variable "project_id" {
+  description = "The project ID to host the cluster in"
+}
+variable "cluster_name" {
+  description = "The name for the GKE cluster"
+  default     = "faster-codes-backend"
+}
+
+variable "network" {
+  description = "The VPC network created to host the cluster in"
+  default     = "gke-network"
+}
+variable "subnetwork" {
+  description = "The subnetwork created to host the cluster in"
+  default     = "gke-subnet"
+}
+variable "ip_range_pods_name" {
+  description = "The secondary ip range to use for pods"
+  default     = "ip-range-pods"
+}
+variable "ip_range_services_name" {
+  description = "The secondary ip range to use for services"
+  default     = "ip-range-services"
+}
