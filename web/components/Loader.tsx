83ffa11461a1735758667a1da65de3285c42791c
Anurag Ashok <anurag16890@gmail.com>
2022-02-05 14:56:24 +0000

    fix typo

--- web/components/Loader.tsx
+++ web/components/Loader.tsx
@@ -5,8 +5,8 @@ const Loader: React.FC = () => {
         <div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-2 mt-2  m-auto"></div>
         <div>executing code and generating statistics</div>
         <div>
-          tired of waiting? bookmark the page and come back later..
-          the results
+          tired of waiting? bookmark the page and come back later to view the
+          results
         </div>
       </div>
     </div>
