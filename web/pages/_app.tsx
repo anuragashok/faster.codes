4ad5c7eb0ad18807fa2e848d7ac22fe0c40b9a61
Anurag Ashok <anurag16890@gmail.com>
2022-02-23 10:53:28 +0000

    add gtag

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -2,9 +2,27 @@ import "@styles/globals.css";
 import type { AppProps } from "next/app";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
+import Script from "next/script";
 config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-N5QGMDMSW1"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+window.dataLayer = window.dataLayer || [];
+function gtag(){window.dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-N5QGMDMSW1');
+`}
+      </Script>
+      <Component {...pageProps} />;
+    </>
+  );
 }
 export default MyApp;
--- web/pages/_document.tsx
+++ web/pages/_document.tsx
@@ -18,19 +18,6 @@ class MyDocument extends Document {
           />
         </Head>
         <body>
-          <Script
-            src="https://www.googletagmanager.com/gtag/js?id=G-N5QGMDMSW1"
-            strategy="afterInteractive"
-          />
-          <Script id="google-analytics" strategy="afterInteractive">
-            {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-N5QGMDMSW1');
-        `}
-          </Script>
           <Main />
           <NextScript />
         </body>
