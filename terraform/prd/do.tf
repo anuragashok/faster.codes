0008b2adfe4547dda153a3511a7ca7439b6b4da4
Anurag Ashok <anurag16890@gmail.com>
2022-05-12 09:19:05 +0000

    rename data store

--- terraform/prd/do.tf
+++ terraform/prd/do.tf
@@ -16,7 +16,7 @@ data "digitalocean_kubernetes_cluster" "faster_codes" {
   name = digitalocean_kubernetes_cluster.faster_codes.name
 }
 
-resource "digitalocean_spaces_bucket" "faster_codes_run_data_store" {
+resource "digitalocean_spaces_bucket" "faster_codes_data_store" {
   name   = var.spaces_bucket_name
   region = "ams3"
   acl    = "private"
