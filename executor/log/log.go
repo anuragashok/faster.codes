744ef7154550b236b674e68cdf2775fa4d5bd8b4
Anurag Ashok <anurag16890@gmail.com>
2022-05-15 12:59:43 +0000

    fix logger config

--- executor/log/log.go
+++ executor/log/log.go
@@ -3,8 +3,10 @@ package log
 import (
 	"context"
 	"encoding/json"
+	"os"
 
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 var (
@@ -12,30 +14,43 @@ var (
 	sugar  *zap.SugaredLogger
 
 	//tags
-	RUN_ID = "run_id"
-	CODE_RUN_ID = "code_run_id"
+	RUN_ID string = "run_id"
+	CODE_RUN_ID string = "code_run_id"
 
 	TAG_KEYS = []string{RUN_ID,CODE_RUN_ID}
 )
 
 func Init() func() {
-	logger, _ = zap.NewProduction()
+	atom := zap.NewAtomicLevel()
+	encoderCfg := zap.NewProductionEncoderConfig()
+	encoderCfg.TimeKey = "timestamp"
+	encoderCfg.EncodeTime = zapcore.ISO8601TimeEncoder
+	logger := zap.New(zapcore.NewCore(
+		zapcore.NewJSONEncoder(encoderCfg),
+		zapcore.Lock(os.Stdout),
+		atom,
+	))
 	sugar = logger.Sugar()
+	
 	return func() {
 		sugar.Sync()
 	}
 }
 
-func Info(ctx context.Context, message string, additionalTags ...string) {
-	sugar.Infow(message, getTags(ctx), additionalTags)
+func Info(ctx context.Context, message string, additionalTags ...interface{}) {
+	tags := []interface{}{}
+	tags = append(getTags(ctx), additionalTags...)
+	sugar.Infow(message, tags)
 }
 
-func Error(ctx context.Context, err error, additionalTags ...string) {
-	sugar.Errorw(err.Error(), getTags(ctx), additionalTags)
+func Error(ctx context.Context, err error, additionalTags ...interface{}) {
+	tags := []interface{}{}
+	tags = append(getTags(ctx), additionalTags...)
+	sugar.Errorw(err.Error(), tags)
 }
 
-func getTags(ctx context.Context) []string {
-	tags := []string{}
+func getTags(ctx context.Context) []interface{} {
+	tags := []interface{}{}
 	for _, k := range TAG_KEYS {
 		v := ctx.Value(k)
 		str, ok := v.(string)
